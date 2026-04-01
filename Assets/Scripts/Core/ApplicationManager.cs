c9c8c1306168f30b0492de8e69b986aa549692e9
Eli Koederitz <bkoederitz@mail.bradley.edu>
2026-04-01 14:26:08 -0500

    Manager Service Setup

---
+++ Assets/Scripts/Core/ApplicationManager.cs
@@ -0,0 +1,48 @@
+/*****************************************************************************
+// File Name : ApplicationManager.cs
+// Author : Brandon Koederitz
+// Creation Date : 4/1/2026
+// Last Modified : 4/1/2026
+//
+// Brief Description : Manages initializing all manager systems.
+*****************************************************************************/
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class ApplicationManager : MonoBehaviour
+{
+    [SerializeField] private Manager[] managers;
+
+    private Manager[] managerInstances;
+
+    /// <summary>
+    /// Initialize all managers on awake.
+    /// </summary>
+    private async Awaitable Awake()
+    {
+        managerInstances = new Manager[managers.Length];
+        for(int i = 0; i < managers.Length; i++)
+        {
+            Manager inst = Instantiate(managers[i], transform);
+            managerInstances[i] = inst;
+            await inst.Initialize();
+        }
+    }
+
+    /// <summary>
+    /// Gets a specific manager instance by type.
+    /// </summary>
+    /// <typeparam name="T">The type of the manager instance to get.</typeparam>
+    /// <returns>The found manager.</returns>
+    public T GetManager<T>() where T : Manager
+    {
+        foreach(var manager in managerInstances)
+        {
+            if (manager is T typedInst)
+            {
+                return typedInst;
+            }
+        }
+        return null;
+    }
+}
