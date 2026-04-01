71b15e15e04c25f19f4b239f9ed01bf92bd5ab03
Eli Koederitz <bkoederitz@mail.bradley.edu>
2026-04-01 14:48:25 -0500

    Start Input Service

---
+++ Assets/Scripts/DecisionSystem/InputService.cs
@@ -0,0 +1,15 @@
+/*****************************************************************************
+// File Name : SequencerService.cs
+// Author : Brandon Koederitz
+// Creation Date : 4/1/2026
+// Last Modified : 4/1/2026
+//
+// Brief Description : Controls the sequence of moving through decision points on the tree.
+*****************************************************************************/
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputService : DecisionService
+{
+    
+}
