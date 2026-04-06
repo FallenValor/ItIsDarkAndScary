c96293ad2173972c23e2d1d541ef8946b01b1c05
Eli Koederitz <bkoederitz@mail.bradley.edu>
2026-04-06 14:42:16 -0500

    TimerService

--- Assets/Scripts/DecisionSystem/PlayerControllerService.cs
+++ Assets/Scripts/DecisionSystem/PlayerControllerService.cs
@@ -90,14 +90,17 @@ namespace IDAS.Decisions
         /// </summary>
         /// <param name="splineIndex">The index of the subsequent node/spline to move to.</param>
         /// <returns></returns>
-        public async Awaitable MoveToPoint(NodePoint startPoint, int splineIndex, NodePoint endNode, CancellationToken ct)
+        public async Awaitable MoveToPoint(NodePoint startPoint, int splineIndex, NodePoint endPoint, CancellationToken ct)
         {
             // Get the spline to move along.
-            SplineContainer spline = startPoint.Splines[splineIndex];
-            if (spline == null)
+            if (splineIndex > startPoint.Splines.Length || startPoint.Splines[splineIndex] == null)
             {
+                Debug.LogWarning($"No Spline found connecting nodes {startPoint} and {endPoint}.  " +
+                    $"Using default Cinemachine interpolation.");
+                endPoint.CCam.Prioritize();
                 return;
             }
+            SplineContainer spline = startPoint.Splines[splineIndex];
 
             //Update the player.
             player.CameraPosition = 0;
@@ -115,7 +118,7 @@ namespace IDAS.Decisions
                 await Awaitable.NextFrameAsync();
             }
 
-            endNode.CCam.Prioritize();
+            endPoint.CCam.Prioritize();
             Debug.Log("Hit end of track");
         }
     }
--- Assets/Scripts/DecisionSystem/TimerService.cs
+++ Assets/Scripts/DecisionSystem/TimerService.cs
@@ -7,25 +7,114 @@
 // Brief Description : Manages adding a time limit to player decisions.
 *****************************************************************************/
 using System;
+using System.Threading;
 using UnityEngine;
 
 namespace IDAS.Decisions
 {
     public class TimerService : DecisionService
     {
+        [SerializeField] private float decisionTime;
+
+        private CancellationTokenSource cts;
+        private bool isRunning;
+
         #region Events
         public event Action<float, float> TimerUpdateEvent;
         public event Action TimerCompleteEvent;
+        public event Action TimerCancelEvent;
         #endregion
 
+        /// <summary>
+        /// Starts the timer counting down.
+        /// </summary>
+        /// <param name="time"></param>
+        public void StartTimer()
+        {
+            StartTimer(decisionTime);
+        }
         public void StartTimer(float time)
         {
-
+            if (isRunning)
+            {
+                StopTimerInternal();
+            }
+            cts = new CancellationTokenSource();
+            isRunning = true;
+            TimerAsync(time, cts.Token);
         }
 
+        /// <summary>
+        /// Cancel the timer on Deinitialize
+        /// </summary>
+        public override void Deinitialize()
+        {
+            if(cts != null)
+            {
+                cts.Cancel();
+            }
+        } 
+
+        /// <summary>
+        /// Stops the timer.
+        /// </summary>
         public void StopTimer()
         {
+            if (!isRunning) { return; }
+            StopTimerInternal();
+            TimerCancelEvent?.Invoke();
+        }
 
+        /// <summary>
+        /// Actually stops the timer, skipping event calls.
+        /// </summary>
+        private void StopTimerInternal()
+        {
+            if (!isRunning) { return; }
+            cts.Cancel();
+        }
+
+        /// <summary>
+        /// Cleans up the timer and returns the TimerService to a static state.
+        /// </summary>
+        private void CleanUpTimer()
+        {
+            isRunning = false;
+            cts = null;
+        }
+
+        /// <summary>
+        /// Asynchronously runs the timer function.
+        /// </summary>
+        /// <param name="time"></param>
+        /// <param name="ct"></param>
+        private async void TimerAsync(float time, CancellationToken ct)
+        {
+            float timer = time;
+            try
+            {
+                while (timer > 0)
+                {
+                    if (ct.IsCancellationRequested)
+                    {
+                        CleanUpTimer();
+                        return;
+                    }
+
+                    timer -= Time.deltaTime;
+                    TimerUpdateEvent?.Invoke(timer, timer / time);
+
+                    await Awaitable.NextFrameAsync(ct);
+                }
+                // Timer Complete.
+                CleanUpTimer();
+                TimerCompleteEvent?.Invoke();
+                Debug.Log("Timer Complete.");
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+            }
         }
     }
 }
