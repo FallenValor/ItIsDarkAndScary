60646596142f8ff32a0188a8d3624a920b93ac70
Eli Koederitz <bkoederitz@mail.bradley.edu>
2026-04-21 14:45:53 -0500

    Audio Manager and Relays

---
+++ Assets/Scripts/Audio/CustomRelays/DecisionAudioRelay.cs
@@ -0,0 +1,45 @@
+/*****************************************************************************
+// File Name : DecisionAudioRelay.cs
+// Author : Brandon Koederitz
+// Creation Date : 4/21/2026
+// Last Modified : 4/21/2026
+//
+// Brief Description : Plays different sounds when the player makes decisions.
+*****************************************************************************/
+using IDAS.Decisions;
+using IDAS.Decisions.Tree;
+using UnityEngine;
+
+namespace IDAS.Audio
+{
+    public class DecisionAudioRelay : AudioRelay
+    {
+        [SerializeField] private string staminaChoiceSound;
+        [SerializeField] private string normalChoiceSound;
+        private void Awake()
+        {
+            DecisionTreeService dts = GetComponent<DecisionTreeService>();
+            dts.MakeDecisionEvent += PlayDecisionSound;
+        }
+
+        private void OnDestroy()
+        {
+            DecisionTreeService dts = GetComponent<DecisionTreeService>();
+            dts.MakeDecisionEvent -= PlayDecisionSound;
+        }
+
+        private void PlayDecisionSound(DarkScaryNode currentNode, int decisionIndex, DarkScaryNode nextNode)
+        {
+            DecisionNode dNode = currentNode as DecisionNode;
+            Choice choice = dNode.Choices[decisionIndex];
+            if (choice.Stamina > 0)
+            {
+                PlayOneShot(staminaChoiceSound);
+            }
+            else
+            {
+                PlayOneShot(normalChoiceSound);
+            }
+        }
+    }
+}
